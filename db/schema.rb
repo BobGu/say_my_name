e95915eb5e74715040113d2b667c0e2ff4a0d553
Robert Gu <verygudhokie@gmail.com>
2014-11-11 13:19:38 -0700

    added cohort to students table so I can filter by cohort later

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111141539) do
+ActiveRecord::Schema.define(version: 20141111201644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141111141539) do
   create_table "students", force: true do |t|
     t.string "name"
     t.string "picture"
+    t.string "cohort"
   end
 
 end
