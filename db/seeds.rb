e72766eeb5e08bf0ead62840cbcf892447228248
Robert Gu <verygudhokie@gmail.com>
2014-11-23 09:45:44 -0700

    changed seeds file for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-CSV.foreach('/Users/robertgu/turing/say_my_name/app/assets/images/turing_students.csv') do |row|
+CSV.foreach('app/assets/images/turing_students.csv') do |row|
   if row[0] != nil
     Student.create(name: row[0], picture: "#{row[0]} #{row[1]}", cohort: row[2])
   end
